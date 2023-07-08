de100a6265fddc2e42c5b7c849d8a9d729183b10
AlmadelX <almadelx@proton.me>
2023-07-08 19:55:26 +0500

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hi!");
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3);
+  const moves = ["Rock", "Paper", "Scissors"];
+
+  return moves[choice];
+}
+
+console.log(getComputerChoice());
